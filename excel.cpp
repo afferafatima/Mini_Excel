3abb006ba827eab832fb191886953ff5bb484948
Afeera Fatima <afferafatima555@gmail.com>
2023-10-22 19:56:46 +0500

    added a function

--- excel.cpp
+++ excel.cpp
@@ -27,12 +27,81 @@ class Excel
             down = d;
         }
     };
+
     Cell* current;
+    Cell* head;
 	vector<vector<string>> grid;
     int row_size, col_size;         //row and colomn size at runtime
 	int c_row, c_col;               //current row and current colomn
     const int cellWidth = 10;       //width of each cell at console
 	const int cellHeight = 4;       //height of each cell at console
 
+public:
+    Excel()
+	{
+		head = nullptr;
+		current = nullptr;
+		row_size = col_size = 5;
+		c_row = c_col = 0;
 
+		head = New_row();
+		current = head;
+		for (int i = 0; i < row_size - 1; i++)
+		{
+			InsertRowBelow();
+			row_size--;
+			current = current->down;
+		}
+
+		current = head;
+		
+	}
+    Cell* New_row()
+	{
+		Cell* temp = new Cell(); //cell at the start of the row i.e 0 index
+		Cell* curr = temp;
+		for (int i = 0; i < col_size - 1; i++)
+		{
+			Cell* temp2 = new Cell();
+			temp->right = temp2;
+			temp2->left = temp;
+			temp = temp2;
+		}
+		return curr;
+	}
+    void InsertRowBelow()
+	{
+		Cell* temp = New_row();
+		Cell* temp2 = current;
+
+		while (temp2->left != nullptr)
+		{
+			temp2 = temp2->left;
+		}
+		if (temp2->down == nullptr)
+		{
+			while (temp2 != nullptr)
+			{
+				temp2->down = temp;
+				temp->up = temp2;
+
+				temp = temp->right;
+				temp2 = temp2->right;
+			}
+		}
+		else
+		{
+			while (temp2 != nullptr)
+			{
+				temp->down = temp2->down;
+				temp2->down = temp;
+				temp->up = temp2;
+				temp->down->up = temp;
+
+				temp = temp->right;
+				temp2 = temp2->right;
+			}
+		}
+		row_size++;
+	}
 };
