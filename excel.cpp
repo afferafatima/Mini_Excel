258d248cb438b5c916c9c3f3ef3819503c35a18e
Afeera Fatima <afferafatima555@gmail.com>
2023-10-27 01:40:06 +0500

    Grid printed and movement.

--- excel.cpp
+++ excel.cpp
@@ -1,87 +1,251 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include <windows.h>
 #include <conio.h>
-#include <iomanip>
-#include <fstream>
-#include <string>
-#include <vector>
 using namespace std;
+struct Cordinate
+{
+	int row = 0;
+	int col = 0;
+};
 class Excel
 {
-	class Cell
-	{
-		friend class Excel;
-		string data;
-		Cell *up;
-		Cell *down;
-		Cell *left;
-		Cell *right;
+    class Cell
+    {
+        friend class Excel;
+        string data;
+        Cell *up;
+        Cell *down;
+        Cell *left;
+        Cell *right;
 
-	public:
-		Cell(string input = " ", Cell *l = nullptr, Cell *r = nullptr, Cell *u = nullptr, Cell *d = nullptr)
-		{
-			data = input;
-			left = l;
-			right = r;
-			up = u;
-			down = d;
-		}
-	};
+    public:
+        Cell(string input = " ", Cell *l = nullptr, Cell *r = nullptr, Cell *u = nullptr, Cell *d = nullptr)
+        {
+            data = input;
+            left = l;
+            right = r;
+            up = u;
+            down = d;
+        }
+    };
 
-	Cell *current;
-	Cell *head;
+    Cell* current;	
+    Cell* head;
 	vector<vector<string>> grid;
-	int row_size, col_size;	  // row and colomn size at runtime
-	int c_row, c_col;		  // current row and current colomn
-	const int cellWidth = 10; // width of each cell at console
-	const int cellHeight = 4; // height of each cell at console
+    int rowSize, colSize;         //row and colomn size at runtime
+	int currentRow, currentCol;               //current row and current colomn
+	/*for excel functions like sum,average*/
+	Cell* RangeStart;
+	Cordinate Range_start{};  
+	Cordinate Range_end{};
+	/*to print cell on console*/
+    const int cellWidth = 10;       //width of each cell at console
+	const int cellHeight = 4;       //height of each cell at console
+
+	void color(int k)
+	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleTextAttribute(hConsole, k);
+		if (k > 255)
+		{
+			k = 1;
+		}
+	}
+
+	void gotoxy(int rpos, int cpos)
+{
+	COORD scrn;
+	HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+	scrn.X = cpos;
+	scrn.Y = rpos;
+	SetConsoleCursorPosition(hOuput, scrn);
+}
+	
+	//print cell
+	void printCell(int row, int col, int colour)
+	{
+		color(colour);
+		char c = '*';
+		// top horizontal line
+		gotoxy(row * cellHeight, col * cellWidth);
+		for (int i = 0; i < cellWidth; i++)
+		{
+			cout << c;
+		}
+
+		// Print bottom horizontal line
+		gotoxy(row * cellHeight + cellHeight, col * cellWidth);
+		for (int i = 0; i <= cellWidth; i++)
+		{
+			cout << c;
+		}
+
+		// Print vertical lines on both sides
+		for (int i = 0; i < cellHeight; i++)
+		{
+
+			gotoxy(row * cellHeight + i, col * cellWidth);
+			cout << c;
+		}
+
+		int r = row * cellHeight;
+		int ci = (col * cellWidth) + cellWidth;
+		for (int i = 0; i <= cellHeight; i++)
+		{
+
+			gotoxy(r + i, ci);
+			cout << c;
+		}
+
+		//Clear the center of the cell
+		gotoxy((cellHeight * row) + cellHeight / 2, (col * cellWidth) + cellWidth / 2);
+		cout << "     ";
+	}
+
+	void printCellData(int row, int col, Cell* d, int colour)
+	{
+		color(colour);
+		gotoxy((cellHeight * row) + cellHeight / 2, (col * cellWidth) + cellWidth / 2);
+		cout << d->data;
+	}
+
+
+	void printCol()
+	{
+		for (int ri = 0, ci = currentCol + 1; ri < rowSize; ri++)
+		{
+			printCell(ri, ci, 7);
+		}
+	}
+
+	void printRow()
+	{
+		for (int ci = 0, ri = currentRow + 1; ci < colSize; ci++)
+		{
+			printCell(ri, ci, 7);
+		}
+
+	}
+
 
 public:
-	Excel()
+class iterator
+	{
+		Cell* t;
+		friend class Excel;
+		iterator()
+		{
+			t = nullptr;
+		}
+
+		iterator(Cell* data)
+		{
+			t = data;
+		}
+
+		iterator operator++()
+		{
+			if (t->right != nullptr)
+				t = t->right;
+			return *this;
+		}
+
+		iterator operator++(int)
+		{
+			if (t->down != nullptr)
+				t = t->down;
+			return *this;
+		}
+
+		iterator operator--()
+		{
+			if (t->left != nullptr)
+				t = t->left;
+			return *this;
+		}
+
+		iterator operator--(int)
+		{
+			if (t->right != nullptr)
+				t = t->right;
+			return *this;
+		}
+
+		bool operator==(iterator temp)
+		{
+			return (t == temp.t);
+		}
+
+		bool operator!=(iterator temp)
+		{
+			return (t != temp.t);
+		}
+
+		string& operator*()
+		{
+			return t->data;
+		}
+
+	};
+
+	iterator Get_Head()
+	{
+		return iterator(head);
+	}
+
+    Excel()
 	{
 		head = nullptr;
 		current = nullptr;
-		row_size = col_size = 5;
-		c_row = c_col = 0;
+		rowSize = colSize = 5;
+		currentRow = currentCol = 0;
 
-		head = NewRow();
+		head = newRow();
 		current = head;
-		for (int i = 0; i < row_size - 1; i++)
+		for (int i = 0; i < rowSize - 1; i++)
 		{
-			InsertRowBelow();
-			row_size--;
+			insertRowBelow();
+			rowSize--;
 			current = current->down;
 		}
 
 		current = head;
+		cout<<cellWidth<<endl;
+		cout<<cellHeight<<endl;
+		getch();
+		printGrid();
 	}
-	Cell *NewRow()
+    Cell* newRow()
 	{
-		Cell *temp = new Cell(); // cell at the start of the row i.e 0 index
-		Cell *curr = temp;
-		for (int i = 0; i < col_size - 1; i++)
+		Cell* temp = new Cell(); //cell at the start of the row i.e 0 index
+		Cell* curr = temp;
+		for (int i = 0; i < colSize - 1; i++)
 		{
-			Cell *temp2 = new Cell();
+			Cell* temp2 = new Cell();
 			temp->right = temp2;
 			temp2->left = temp;
 			temp = temp2;
 		}
 		return curr;
 	}
-	// Insert row below the row containing current cell
-	void InsertRowBelow()
+	//insert row below the row containing current cell 
+    void insertRowBelow()
 	{
-		Cell *temp = NewRow(); // creates a new row of of colomn length
-		Cell *temp2 = current; // current cell
+		Cell* temp = newRow(); // Create a new row of column length
+		Cell* temp2 = current; // Store the current cell for navigation
 
-		// to go left most side of sheet
+		// Move to the leftmost side of the sheet
 		while (temp2->left != nullptr)
 		{
 			temp2 = temp2->left;
 		}
-		// if last row
+
+		// If it's the last row
 		if (temp2->down == nullptr)
 		{
+			// Link the new row below the last row
 			while (temp2 != nullptr)
 			{
 				temp2->down = temp;
@@ -91,8 +255,9 @@ public:
 				temp2 = temp2->right;
 			}
 		}
-		else
+		else // If it's not the last row
 		{
+			// insert the new row between existing rows
 			while (temp2 != nullptr)
 			{
 				temp->down = temp2->down;
@@ -104,28 +269,32 @@ public:
 				temp2 = temp2->right;
 			}
 		}
-		row_size++;
+		rowSize++; // Increment the row count
+		
 	}
-	// Insert row above the row containing current cell
-	void InsertRowAbove()
+
+	// Create a new row to insert above the row containing the current cell
+	void insertRowAbove()
 	{
+		Cell* temp = newRow();  // Create a new row of cells
+		Cell* temp2 = current;  // Store the current cell for navigation
 
-		Cell *temp = NewRow();
-		Cell *temp2 = current;
-
-		// to go left most side of sheet
+		// Move to the leftmost side of the sheet
 		while (temp2->left != nullptr)
 		{
 			temp2 = temp2->left;
 		}
 
+		// If the current cell is the leftmost top cell, update the head of the sheet
 		if (temp2 == head)
 		{
 			head = temp;
 		}
-		// if added new row above the top row
+
+		// If the new row is inserted above the top row
 		if (temp2->up == nullptr)
 		{
+			// Link the new row above the current row
 			while (temp2 != nullptr)
 			{
 				temp2->up = temp;
@@ -135,8 +304,9 @@ public:
 				temp2 = temp2->right;
 			}
 		}
-		else
+		else // If it's not the top row
 		{
+			// insert the new row between the existing rows
 			while (temp2 != nullptr)
 			{
 				temp->up = temp2->up;
@@ -148,16 +318,21 @@ public:
 				temp2 = temp2->right;
 			}
 		}
-		row_size++;
-	}
 
-	Cell *NewCol()
+		rowSize++;  
+		printGrid();  // Update and display the grid
+		// Print_Data(); // print data
+	}
+	//insert new colomn
+	Cell* newCol()
 	{
-		Cell *temp = new Cell(); // cell at the start of the colomn
-		Cell *curr = temp;
-		for (int i = 0; i < row_size - 1; i++)
+		Cell* temp = new Cell();//cell at the start of the colomn
+		Cell* curr = temp;
+
+		// Create and link new cells for the column
+		for (int i = 0; i < rowSize - 1; i++)
 		{
-			Cell *temp2 = new Cell();
+			Cell* temp2 = new Cell();
 			temp->down = temp2;
 			temp2->up = temp;
 
@@ -166,17 +341,17 @@ public:
 
 		return curr;
 	}
-	// insert colomn to the right of colomn containing currrent cell
-	void InsertColRight()
+	//insert colomn to the right of colomn containing currrent cell
+	void insertColRight()
 	{
-		Cell *temp = NewCol();
-		Cell *temp2 = current;
-		// to go to the top of the colomn
+		Cell* temp = newCol();
+		Cell* temp2 = current;
+		// Move to the rightmost side of the sheet
 		while (temp2->up != nullptr)
 		{
 			temp2 = temp2->up;
 		}
-		// if colomn is added to right to the right most colomn
+		//if colomn is inserted right to the right most colomn
 		if (temp2->right == nullptr)
 		{
 			while (temp2 != nullptr)
@@ -188,7 +363,7 @@ public:
 				temp = temp->down;
 			}
 		}
-		else
+		else	
 		{
 			while (temp2 != nullptr)
 			{
@@ -202,26 +377,27 @@ public:
 			}
 		}
 
-		col_size++;
+		colSize++;
+
 	}
-	// insert colomn to the right of colomn containing currrent cell
-	void InsertColLeft()
+	//insert colomn to the right of colomn containing currrent cell
+	void insertColLeft()
 	{
 
-		Cell *temp = NewCol();
-		Cell *temp2 = current;
+		Cell* temp = newCol();
+		Cell* temp2 = current;
 
-		// to go to the top
+		// Move to the leftmost side of the sheet
 		while (temp2->up != nullptr)
 		{
 			temp2 = temp2->up;
 		}
-
+		// If the current cell is the leftmost top cell, update the head of the sheet
 		if (temp2 == head)
 		{
 			head = temp;
 		}
-		// if colomn is added to the left to left most colomn
+		//if colomn is inserted left to left most colomn
 		if (temp2->left == nullptr)
 		{
 			while (temp2 != nullptr)
@@ -233,7 +409,7 @@ public:
 				temp = temp->down;
 			}
 		}
-		else
+		else 
 		{
 			while (temp2 != nullptr)
 			{
@@ -247,19 +423,21 @@ public:
 			}
 		}
 
-		col_size++;
+		colSize++;
+		 printGrid();
+		// Print_Data();
 	}
-	// insert new cell in a row
-	void InsertCellByDownShift()
+	//insert new cell in a row
+	void insertCellByDownShift()
 	{
-		Cell *temp = current;
+		Cell* temp = current;
 		while (current->down != nullptr)
 		{
 			current = current->down;
 		}
-		InsertRowBelow();
+		insertRowBelow();
 		current = current->down;
-
+		//move data
 		while (current != temp)
 		{
 			current->data = current->up->data;
@@ -267,15 +445,15 @@ public:
 		}
 		current->data = " ";
 	}
-	// insert new cell in a colomn
-	void InsertCellByRightShift()
+	//insert new cell in a colomn
+	void insertCellByRightShift()
 	{
-		Cell *temp = current;
+		Cell* temp = current;
 		while (current->right != nullptr)
 		{
 			current = current->right;
 		}
-		InsertColRight();
+		insertColRight();
 		current = current->right;
 
 		while (current != temp)
@@ -285,23 +463,22 @@ public:
 		}
 		current->data = "   ";
 	}
-
-	// delete cell data as a row
-	void DeleteCellByLeftShift()
+	//delete cell data as a row
+	void deleteCellByLeftShift()
 	{
-		Cell *temp = current;
+		Cell* temp = current;
 		temp->data = "    ";
-		while (temp->right != nullptr)
+		while (temp->right!= nullptr)
 		{
 			temp->data = temp->right->data;
 			temp = temp->right;
 		}
 		temp->data = "    ";
 	}
-	// delete cell data as a colom
-	void DeleteCellByUpShift()
+	//delete cell data as a colom
+	void deleteCellByUpShift()
 	{
-		Cell *temp = current;
+		Cell* temp = current;
 		temp->data = " ";
 		while (temp->down != nullptr)
 		{
@@ -310,4 +487,241 @@ public:
 		}
 		temp->data = " ";
 	}
+	//delete current colomn
+	void deleteCol()
+	{
+		if (colSize <= 1)//if only one colomn
+			return;
+		Cell* temp = current;
+
+		while (temp->up != nullptr)
+		{
+			temp = temp->up;
+		}
+
+
+		if (temp == head)
+		{
+			head = temp->right;
+		}
+
+		Cell* delete_cell;
+		//delete left most colomn
+		if (temp->left == nullptr)
+		{
+			current = current->right;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->right->left = nullptr;
+
+				temp = temp->down;
+				delete delete_cell;
+			}
+
+		}
+		//delete right most colomn
+		else if (temp->right == nullptr)
+		{
+			currentCol--;
+			current = current->left;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->left->right = nullptr;
+				temp = temp->down;
+				delete delete_cell;
+			}
+		}
+		else
+		{
+			currentCol--;
+			current = current->left;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->left->right = temp->right;
+				temp->right->left = temp->left;
+
+				temp = temp->down;
+				delete delete_cell;
+			}
+		}
+		colSize--;
+	}
+	//delete current row
+	void deleteRow()
+	{
+
+		if (rowSize <= 1)
+			return;
+		Cell* temp = current;
+
+		while (temp->left != nullptr)
+		{
+			temp = temp->left;
+		}
+
+
+		if (temp == head)
+		{
+			head = temp->down;
+		}
+
+		Cell* delete_cell;
+		//delete top most row
+		if (temp->up == nullptr)
+		{
+			current = current->down;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->down->up = nullptr;
+
+				temp = temp->right;
+				delete delete_cell;
+			}
+
+		}
+		//delete bottom row
+		else if (temp->down == nullptr)
+		{
+			currentRow--;
+			current = current->up;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->up->down = nullptr;
+				temp = temp->right;
+				delete delete_cell;
+			}
+		}
+		else
+		{
+			currentRow--;
+			current = current->up;
+			while (temp != nullptr)
+			{
+				delete_cell = temp;
+				temp->down->up = temp->up;
+				temp->up->down = temp->down;
+
+				temp = temp->right;
+				delete delete_cell;
+			}
+		}
+		rowSize--;
+	}
+	//clear colomn data
+	void clearCol()
+	{
+		Cell* temp = current;
+		//goto the top of the colomn to start
+		while (temp->up != nullptr)
+		{
+			temp = temp->up;
+		}
+		//clear data
+		while (temp != nullptr)
+		{
+			temp->data="    ";
+			temp = temp->down;
+		}
+
+	}
+	//clear row data
+	void clearRow()
+	{
+		Cell* temp = current;
+		//goto the left most side of row to start
+		while (temp->left != nullptr)
+		{
+			temp = temp->left;
+		}
+		//clear data
+		while (temp != nullptr)
+		{
+			temp->data="    ";
+			temp = temp->right;
+		}
+
+	}
+	
+	void printGrid()
+	{
+		printCell(0, 0, 7);
+		getch();
+		for (int ri = 0; ri < rowSize; ri++)
+		{
+			for (int ci = 0; ci < colSize; ci++)
+			{
+				printCell(ri, ci, 7);
+			}
+		}
+	}
+
+	void Keyboard()
+	{
+		printCell(currentRow, currentCol, 4);
+		Cell* temp = current;
+		string input;
+		while (true)
+		{
+			Sleep(100);
+			if(GetAsyncKeyState(VK_LEFT))
+			{
+				if((currentCol-1)>=0)
+				{
+					current=current->left;
+					printCell(currentRow,currentCol,7);
+					currentCol--;
+					printCell(currentRow,currentCol,4);
+				}
+
+			}
+			if(GetAsyncKeyState(VK_RIGHT))
+			{
+				if((currentCol+1)<colSize)
+				{
+					current=current->right;
+					printCell(currentRow,currentCol,7);
+					currentCol++;
+					printCell(currentRow,currentCol,4);
+				}
+
+			}
+			if(GetAsyncKeyState(VK_UP))
+			{
+				if((currentRow-1)>=0)
+				{
+					current=current->up;
+					printCell(currentRow,currentCol,7);
+					currentRow--;
+					printCell(currentRow,currentCol,4);
+				}
+
+			}
+			if(GetAsyncKeyState(VK_DOWN))
+			{
+				if((currentRow+1)<rowSize)
+				{
+					current=current->down;
+					printCell(currentRow,currentCol,7);
+					currentRow++;
+					printCell(currentRow,currentCol,4);
+				}
+
+			}
+			
+		}
+
+	}
+
 };
+int main()
+{
+	system("cls");
+	Excel data;
+	data.Keyboard();
+	return 0;
+}
